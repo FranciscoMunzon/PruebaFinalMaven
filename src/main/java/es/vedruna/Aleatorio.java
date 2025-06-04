60ba83bd9c035dcc01eeb37bd0cf795150a88aa2
FranciscoMunzon <francisco.munzon@a.vedrunasevillasj.es>
2025-06-04 19:43:51 +0200

    Tercer punto de control

---
+++ src/main/java/es/vedruna/Aleatorio.java
@@ -0,0 +1,32 @@
+package es.vedruna;
+
+import java.util.List;
+import java.util.Random;
+
+public class Aleatorio {
+
+    // Para conseguir el número aleatorio con el rango proporcionado
+    public int numeroAleatorio(int rango) {
+        Random rand = new Random();
+        if (validarRango(rango)) {
+            int resultado = rand.nextInt(rango) + 1;
+            System.out.println("El numero aleatorio es: ");
+            return resultado;
+        }
+        System.out.println("El rango no es válido");
+        return 0;
+    }
+
+    // EXTRA: Si el usuario quiere puede añadir valores para que cuando se elija un número aleatorio te devuelva el valor en vez de un número.
+//    public List listaValores(List<String> valores) {
+//
+//    }
+
+    // Validador para evitar que entren rangos inválidos
+    public boolean validarRango(int rango) {
+        if (rango <= 0) {
+            return false;
+        }
+        return true;
+    }
+}
